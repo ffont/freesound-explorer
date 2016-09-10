ed0a9a465094ca8cca215fe5376934a719ecf2be
giuband <giuseppe.bandiera@live.com>
2016-09-11 00:06:24 +0200

    Add python's range() function

--- static/src/utils/arrayUtils.js
+++ static/src/utils/arrayUtils.js
@@ -15,3 +15,32 @@ export const getRandomElement = (array) => {
   const index = Math.floor(Math.random() * max);
   return array[index];
 };
+
+/**
+ * Emulates python's range() function
+ * @param  {int} param1
+ * @param  {int} param2 (optional)
+ * @param  {int} param3 (optional)
+ * @return {array}
+ */
+export const range = (param1, param2, param3) => {
+  const isValid = (param) => Number.isInteger(param);
+  if (isValid(param1) && isValid(param2) && isValid(param3)) {
+    if (param3 === 0) {
+      throw Error('Third parameter can\'t be 0');
+    }
+    const arrayLength = Math.max(Math.ceil((param2 - param1) / param3), 0);
+    return [...Array(arrayLength).keys()].map(val =>
+      param1 + (val * param3));
+  }
+  if (isValid(param1) && isValid(param2)) {
+    const arrayLength = Math.max(param2 - param1, 0);
+    return [...Array(arrayLength).keys()].map(val =>
+      val + param1);
+  }
+  if (isValid(param1)) {
+    const arrayLength = Math.max(param1, 0);
+    return [...Array(arrayLength).keys()];
+  }
+  throw Error('Range must be called with at least one valid parameter');
+};
--- static/src/utils/arrayUtils.test.js
+++ static/src/utils/arrayUtils.test.js
@@ -38,4 +38,25 @@ describe('array utils', () => {
       expect(utils.indexElementWithId(list, '23', 'name')).toEqual(0);
     });
   });
+  describe('range', () => {
+    it('works as expected with 3 parameters', () => {
+      expect(utils.range(0, 9, 3)).toEqual([0, 3, 6]);
+      expect(utils.range(2, 11, 2)).toEqual([2, 4, 6, 8, 10]);
+      expect(utils.range(2, 10, 2)).toEqual([2, 4, 6, 8]);
+      expect(utils.range(2, 0, 4)).toEqual([]);
+      expect(utils.range(10, 80, 28)).toEqual([10, 38, 66]);
+      expect(utils.range(-6, -1, 2)).toEqual([-6, -4, -2]);
+      expect(utils.range(-6, -1, -2)).toEqual([]);
+      expect(utils.range(1, -4, -2)).toEqual([1, -1, -3]);
+    });
+    it('works as expected with 2 parameters', () => {
+      expect(utils.range(0, 4)).toEqual([0, 1, 2, 3]);
+      expect(utils.range(2, 5)).toEqual([2, 3, 4]);
+      expect(utils.range(-4, -1)).toEqual([-4, -3, -2]);
+    });
+    it('works as expected with 1 parameter', () => {
+      expect(utils.range(4)).toEqual([0, 1, 2, 3]);
+      expect(utils.range(0)).toEqual([]);
+    });
+  });
 });

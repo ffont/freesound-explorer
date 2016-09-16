4b210ad8b6e6f1e72331211ee0db26bbee9fc3f8
giuband <giuseppe.bandiera@live.com>
2016-09-16 20:48:58 +0200

    Add tests for metronome reducer

---
+++ static/src/containers/Metronome/reducer.test.js
@@ -0,0 +1,51 @@
+import expect from 'expect';
+import { updateMetronomeStatus, setTempo, startMetronomeAction,
+  stopMetronomeAction, setPlaySound }
+  from './actions';
+import { default as reducer, initialState } from './reducer';
+
+describe('metronome reducer', () => {
+  it('creates the right initialState', () => {
+    expect(reducer(undefined, {})).toEqual(initialState);
+  });
+  describe('updateMetronomeStatus', () => {
+    const bar = 2;
+    const beat = 3;
+    const tick = 10;
+    const stateAfter = Object.assign({}, initialState,
+     { bar, beat, tick });
+    it('works as expected', () => {
+      expect(reducer(initialState, updateMetronomeStatus(bar, beat, tick)))
+        .toEqual(stateAfter);
+    });
+  });
+  describe('setTempo', () => {
+    const tempo = 130;
+    const stateAfter = Object.assign({}, initialState, { tempo });
+    it('works correctly', () => {
+      expect(reducer(initialState, setTempo(tempo))).toEqual(stateAfter);
+    });
+  });
+  describe('startMetronomeAction', () => {
+    const stateAfter = Object.assign({}, initialState, { isPlaying: true });
+    it('works correctly', () => {
+      expect(reducer(initialState, startMetronomeAction()))
+        .toEqual(stateAfter);
+    });
+  });
+  describe('stopMetronomeAction', () => {
+    const stateAfter = Object.assign({}, initialState, { isPlaying: false });
+    it('works correctly', () => {
+      expect(reducer(initialState, stopMetronomeAction()))
+        .toEqual(stateAfter);
+    });
+  });
+  describe('setPlaySound', () => {
+    const shouldPlaySound = false;
+    const stateAfter = Object.assign({}, initialState, { shouldPlaySound });
+    it('works correctly', () => {
+      expect(reducer(initialState, setPlaySound(shouldPlaySound)))
+        .toEqual(stateAfter);
+    });
+  });
+});

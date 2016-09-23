ff6a6480d75a22a1f5d7761440c8aea6da903a2c
Frederic Font <frederic.font@gmail.com>
2016-09-23 10:04:29 +0200

    Relocate loadJSON util function and add POST capability

---
+++ static/src/utils/requests.js
@@ -0,0 +1,20 @@
+export const loadJSON = (url, postData = undefined) => new Promise((resolve, reject) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open(postData ? 'post' : 'get', url, true);
+  xhr.responseType = 'json';
+  xhr.onload = () => {
+    const { status } = xhr;
+    if (status === 200) {
+      resolve(xhr.response);
+    } else {
+      reject(xhr.response);
+    }
+  };
+  xhr.onerror = () => reject();
+  if (postData) {
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.send(JSON.stringify(postData));
+  } else {
+    xhr.send();
+  }
+});

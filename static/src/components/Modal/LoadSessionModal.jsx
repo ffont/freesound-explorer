808bbd4ef3ff7e01ebfc2f4d574f348ec587e5ba
giuband <giuseppe.bandiera@live.com>
2016-09-24 10:06:25 +0200

    Basic working implementation of session loader

--- static/src/components/Modal/LoadSessionModal.jsx
+++ static/src/components/Modal/LoadSessionModal.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
 
-const LoadSessionModal = () =>
+const modalEntryPropTypes = {
+  session: React.PropTypes.object,
+  loadSession: React.PropTypes.func,
+  userID: React.PropTypes.string,
+};
+
+const ModalEntry = props =>
+  <div onClick={() => props.loadSession(props.session.id, props.userID)}>
+    {props.session.id}
+  </div>;
+ModalEntry.propTypes = modalEntryPropTypes;
+
+
+const propTypes = {
+  sessions: React.PropTypes.array,
+  loadSession: React.PropTypes.func,
+  userID: React.PropTypes.string,
+};
+
+const LoadSessionModal = props =>
   <div className="modal">
-    LoadSessionModal ;)
+    {props.sessions.map(session =>
+      <ModalEntry
+        key={session.id}
+        loadSession={props.loadSession}
+        session={session}
+        userID={props.userID}
+      />
+    )}
   </div>;
 
+LoadSessionModal.propTypes = propTypes;
 export default LoadSessionModal;

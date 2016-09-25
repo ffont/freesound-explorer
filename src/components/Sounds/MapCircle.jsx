d4c8d84d307e581aeb6af441ffed0e3941d9369f
giuband <giuseppe.bandiera@live.com>
2016-09-25 12:40:03 +0200

    Add TweenMax for onPlaying animations

--- src/components/Sounds/MapCircle.jsx
+++ src/components/Sounds/MapCircle.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { lighten } from 'utils/colorsUtils';
-import { mapCircles } from 'stylesheets/variables.json';
+import { mapCircles as circleStyle } from 'stylesheets/variables.json';
 import TweenMax from 'gsap';
 import './MapCircle.scss';
 
@@ -13,7 +13,50 @@ const propTypes = {
   onClick: React.PropTypes.func,
 };
 
+const animationDuration = 1;
+// length of animations that reset initial circle look
+const resetAnimationDuration = 0.3;
+
+const getCircleRadius = () => {
+  const { defaultRadius } = circleStyle;
+  const radius = parseInt(defaultRadius, 10);
+  return radius;
+};
+
 class MapCircle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.tweens = { circle: undefined, filling: undefined };
+  }
+
+  componentWillUpdate(nextProps) {
+    if (nextProps.sound.isPlaying && !this.props.sound.isPlaying) {
+      this.startCircleOnPlayingAnimation();
+    } else if (this.props.sound.isPlaying && !nextProps.sound.isPlaying) {
+      this.stopCircleOnPlayingAnimation();
+    }
+  }
+
+  startCircleOnPlayingAnimation() {
+    const radius = getCircleRadius();
+    this.tweens.circle = TweenMax.to(this.mapCircle, animationDuration, {
+      attr: { r: radius * 2 },
+      repeat: Infinity,
+      yoyo: true,
+    });
+  }
+
+  stopCircleOnPlayingAnimation() {
+    const radius = getCircleRadius();
+    if (this.tweens.circle) {
+      this.tweens.circle.kill();
+      // reset to initial radius
+      this.tweens.circle = TweenMax.to(this.mapCircle, resetAnimationDuration, {
+        attr: { r: radius },
+      });
+    }
+  }
+
   render() {
     const { color, isHovered, isPlaying } = this.props.sound;
     const { isSelected } = this.props;
@@ -21,7 +64,7 @@ class MapCircle extends React.Component {
       this.props.sound.thumbnailPosition : this.props.sound.position;
     const shouldHighlight = (isHovered || isSelected || isPlaying);
     const fillColor = shouldHighlight ? lighten(color, 1.5) : color;
-    const { defaultRadius, defaultStrokeWidth } = mapCircles;
+    const { defaultRadius, defaultStrokeWidth } = circleStyle;
     const fillWidthProportion = 2;
     const radius = parseInt(defaultRadius, 10);
     const strokeWidth = parseInt(defaultStrokeWidth, 10);
@@ -34,7 +77,8 @@ class MapCircle extends React.Component {
         <circle
           cx={cx}
           cy={cy}
-          r={defaultRadius}
+          r={radius}
+          ref={(mapCircle) => { this.mapCircle = mapCircle; }}
           className="MapCircle"
           fill={fillColor}
           stroke={fillColor}
@@ -47,6 +91,7 @@ class MapCircle extends React.Component {
           cy={cy}
           r={mapFillCircleRadius}
           className="MapFillCircle"
+          ref={(fillingCircle) => { this.fillingCircle = fillingCircle; }}
           strokeDasharray={circleLength}
           strokeDashoffset={completionOffset}
           stroke={color}

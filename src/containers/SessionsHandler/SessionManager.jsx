5b1efbd32b3d150361b356f8a63b067b7aca516d
giuband <giuseppe.bandiera@live.com>
2016-09-25 16:00:41 +0200

    Move OptionsList component to separate folder

--- src/containers/SessionsHandler/SessionManager.jsx
+++ src/containers/SessionsHandler/SessionManager.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import OptionsList, { makeOption } from 'components/Input/OptionsList';
+import OptionsList, { makeOption } from 'components/OptionsList';
 import { MODAL_PAGES } from 'constants';
 import { newSession, saveSession, loadSession } from './actions';
 import { setModalPage } from '../Modal/actions';

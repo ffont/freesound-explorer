22a661d1cf2e72d114915fe3f16e2e119de985a5
ffont <frederic.font@gmail.com>
2016-09-28 11:05:03 +0200

    Update metronome reducer
    Separate items in smaller reducers
    
    https://github.com/ffont/freesound-explorer/issues/23

--- src/containers/Metronome/reducer.js
+++ src/containers/Metronome/reducer.js
@@ -12,34 +12,58 @@ export const initialState = {
   shouldPlaySound: false,
 };
 
-function metronome(state = initialState, action) {
+function barReducer(state = initialState.bar, action) {
   switch (action.type) {
-    case UPDATE_METRONOME_STATUS: {
-      return Object.assign({}, state, {
-        bar: action.bar,
-        beat: action.beat,
-        tick: action.tick,
-      });
-    }
-    case SET_TEMPO: {
-      return Object.assign({}, state, {
-        tempo: action.tempo,
-      });
-    }
-    case SET_PLAY_SOUND: {
-      return Object.assign({}, state, {
-        shouldPlaySound: action.shouldPlaySound,
-      });
-    }
-    case START_METRONOME:
-    case STOP_METRONOME: {
-      return Object.assign({}, state, {
-        isPlaying: action.type === START_METRONOME,
-      });
-    }
-    default:
-      return state;
+    case UPDATE_METRONOME_STATUS: return action.bar;
+    default: return state;
   }
 }
 
+function beatReducer(state = initialState.beat, action) {
+  switch (action.type) {
+    case UPDATE_METRONOME_STATUS: return action.beat;
+    default: return state;
+  }
+}
+
+
+function tickReducer(state = initialState.tick, action) {
+  switch (action.type) {
+    case UPDATE_METRONOME_STATUS: return action.tick;
+    default: return state;
+  }
+}
+
+function tempoReducer(state = initialState.tempo, action) {
+  switch (action.type) {
+    case SET_TEMPO: return action.tempo;
+    default: return state;
+  }
+}
+
+function isPlayingReducer(state = initialState.isPlaying, action) {
+  switch (action.type) {
+    case START_METRONOME:
+    case STOP_METRONOME: {
+      return action.type === START_METRONOME;
+    }
+    default: return state;
+  }
+}
+
+function shouldPlaySoundReducer(state = initialState.shouldPlaySound, action) {
+  switch (action.type) {
+    case SET_PLAY_SOUND: return action.shouldPlaySound;
+    default: return state;
+  }
+}
+
+const metronome = (state = initialState, action) => ({
+  bar: barReducer(state.bar, action),
+  beat: beatReducer(state.beat, action),
+  tick: tickReducer(state.tick, action),
+  tempo: tempoReducer(state.tempo, action),
+  isPlaying: isPlayingReducer(state.isPlaying, action),
+  shouldPlaySound: shouldPlaySoundReducer(state.shouldPlaySound, action),
+});
 export default storable(metronome);

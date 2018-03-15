7dbd6808fc5abbd3cb285a4e55fd29e213c58899
Eric Lehmann <coding@kernschall.de>
2018-03-15 14:46:08 +0100

    Copies V01 Selection behaviour from soundlist branch

--- src/containers/Sounds/MapCircleContainer.jsx
+++ src/containers/Sounds/MapCircleContainer.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import MapCircle from 'components/Sounds/MapCircle';
 import { playAudio, stopAudio } from '../Audio/actions';
-import { selectSound, deselectSound, deselectAllSounds,
-  toggleHoveringSound } from './actions';
+import { selectSound, deselectSound, deselectAllSounds, toggleHoveringSound }
+  from './actions';
 import { openModalForSound, hideModal } from '../SoundInfo/actions';
 import { isSoundInsideScreen } from './utils';
 import { makeIsSoundSelected } from './selectors';
 
 const propTypes = {
-  sound: PropTypes.object,
-  isThumbnail: PropTypes.bool,
-  shouldPlayOnHover: PropTypes.bool,
-  isSelected: PropTypes.bool,
-  shouldMultiSelect: PropTypes.bool,
-  playAudio: PropTypes.func,
-  stopAudio: PropTypes.func,
-  selectSound: PropTypes.func,
-  deselectSound: PropTypes.func,
-  deselectAllSounds: PropTypes.func,
-  toggleHoveringSound: PropTypes.func,
-  openModalForSound: PropTypes.func,
-  hideModal: PropTypes.func,
-  soundInfoModal: PropTypes.object,
+  sound: React.PropTypes.object,
+  isThumbnail: React.PropTypes.bool,
+  shouldPlayOnHover: React.PropTypes.bool,
+  isSelected: React.PropTypes.bool,
+  playAudio: React.PropTypes.func,
+  stopAudio: React.PropTypes.func,
+  selectSound: React.PropTypes.func,
+  selectedSounds: React.PropTypes.array,
+  deselectSound: React.PropTypes.func,
+  deselectAllSounds: React.PropTypes.func,
+  toggleHoveringSound: React.PropTypes.func,
+  openModalForSound: React.PropTypes.func,
+  hideModal: React.PropTypes.func,
+  soundInfoModal: React.PropTypes.object,
 };
 
 const isSoundVisible = (props) => {
@@ -65,13 +64,17 @@ class MapCircleContainer extends React.PureComponent {
   }
 
   onClick() {
+    // console.log(this.props.selectedSounds);
+    // console.log('modalID: ' + this.props.soundInfoModal.soundID + 'sel> ' + this.props.isSelected);
+    // console.log('sound: ' + this.props.sound.id + 'pl> ' + this.props.sound.isPlaying);
+    // console.log(this.props.soundInfoModal.soundID === this.props.sound.soundID);
     // play and stop sound
-    if (this.props.sound.isPlaying) {
+    if (!this.props.sound.isPlaying && this.props.isSelected) {
       this.props.stopAudio(this.props.sound);
-    } else if (!(this.props.isSelected || this.props.sound.isPlaying)) {
+    } else {
       this.props.playAudio(this.props.sound);
     }
-    if (this.props.isSelected && !this.props.sound.isPlaying) {
+    if (this.props.isSelected) {
       // toggle selecion
       this.props.deselectSound(this.props.sound.id);
 
@@ -80,19 +83,15 @@ class MapCircleContainer extends React.PureComponent {
         && this.props.soundInfoModal.soundID === this.props.sound.id) {
         this.props.hideModal();
       }
-    } else if (this.props.shouldMultiSelect) {
-      this.props.selectSound(this.props.sound.id);
-      this.props.openModalForSound(this.props.sound);
     } else {
       // toggle selection
-      this.props.deselectAllSounds();
       this.props.selectSound(this.props.sound.id);
 
       // open modal if sound is not yet selected
-      this.props.openModalForSound(this.props.sound);
+        this.props.openModalForSound(this.props.sound);
     }
+  console.log(this.props.selectedSounds);
   }
-
 
   shouldThumbnailUpdate(nextProps) {
     const currentPosition = this.props.sound.thumbnailPosition;
@@ -123,15 +122,16 @@ const makeMapStateToProps = (_, ownProps) => {
   const isSoundSelected = makeIsSoundSelected(soundID);
   return (state) => {
     const sound = state.sounds.byID[soundID];
+    const selectedSounds = state.sounds.selectedSounds;
     const soundInfoModal = state.sounds.soundInfoModal;
-    const { shouldPlayOnHover, shouldMultiSelect } = state.settings;
+    const { shouldPlayOnHover } = state.settings;
     const isSelected = isSoundSelected(state);
     return {
       sound,
+      selectedSounds,
       soundInfoModal,
       isThumbnail,
       shouldPlayOnHover,
-      shouldMultiSelect,
       isSelected,
     };
   };

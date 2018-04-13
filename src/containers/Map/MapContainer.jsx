2d11afad6259f4b78f2aa79657ac9e4cbf63cf6d
Eric Lehmann <coding@kernschall.de>
2018-04-13 15:11:19 +0200

    Adds missing file to the last commit

--- src/containers/Map/MapContainer.jsx
+++ src/containers/Map/MapContainer.jsx
@@ -9,7 +9,7 @@ import { MIN_ZOOM, MAX_ZOOM, PLAY_ON_HOVER_SHORTCUT_KEYCODE } from 'constants';
 import { displaySystemMessage } from '../MessagesBox/actions';
 import { updateMapPosition } from './actions';
 import { setSoundCurrentlyLearnt } from '../Midi/actions';
-import { deselectAllSounds } from '../Sounds/actions';
+import { deselectAllSounds, stopAllSoundsPlaying } from '../Sounds/actions';
 import { hideModal } from '../SoundInfo/actions';
 import Space from '../Spaces/SpaceContainer';
 import SoundInfoContainer from '../SoundInfo/SoundInfoContainer';
@@ -18,6 +18,7 @@ import { setShouldPlayOnHover } from '../Settings/actions';
 
 const propTypes = {
   deselectAllSounds: PropTypes.func,
+  stopAllSoundsPlaying: PropTypes.func,
   paths: PropTypes.array,
   spaces: PropTypes.array,
   map: PropTypes.shape({
@@ -76,6 +77,7 @@ class MapContainer extends React.Component {
     if (evt.target.tagName !== 'circle') {
       // deselect all sounds when not clicking on a circle
       this.props.deselectAllSounds();
+      this.props.stopAllSoundsPlaying();
       // turn off current midi learn
       this.props.setSoundCurrentlyLearnt();
       this.props.hideModal();
@@ -144,6 +146,7 @@ export default connect(mapStateToProps, {
   displaySystemMessage,
   updateMapPosition,
   deselectAllSounds,
+  stopAllSoundsPlaying,
   setSoundCurrentlyLearnt,
   hideModal,
   setShouldPlayOnHover,

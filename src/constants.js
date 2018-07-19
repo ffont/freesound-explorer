f94defbce20b2d3dc82eb0358dc3d3f002e28d47
Eric Lehmann <coding@kernschall.de>
2018-07-19 22:58:43 +0200

    Solves rendom query trigger bug
    When submitting the query form by clicking on the magnifyig glass icon,
    then trying to disable the automation, a new query was fired because we used space,
    now it is "x" when not focusing the input form

--- src/constants.js
+++ src/constants.js
@@ -67,7 +67,7 @@ export const MAP_SCALE_FACTOR = 20;
 export const TOGGLE_MULTISELECTION_KEYCODE = 16; // shift key
 export const PLAY_ON_HOVER_SHORTCUT_KEYCODE = 18; // alt key
 export const TOGGLE_SHOW_CLUSTER_TAGS_KEYCODE = 84; // t
-export const SHORTCUT_ANIMATION_KEYCODE = 32; // space bar
+export const SHORTCUT_ANIMATION_KEYCODE = 88; // x
 
 // tsne
 export const MAX_TSNE_ITERATIONS = 150;

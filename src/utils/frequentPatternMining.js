8517395ad5fdccf74b838c82af4b6ea7556b896c
Eric Lehmann <coding@kernschall.de>
2018-10-14 19:59:45 +0200

    Improves cluster tags

--- src/utils/frequentPatternMining.js
+++ src/utils/frequentPatternMining.js
@@ -1,45 +1,38 @@
 
 // modified version of https://codepen.io/ccallen001/pen/YGOzRA
 function mostFreqStr(arr) {
+  // returns most frequent keys descending by values
+
   // ERROR HANDLING
   // more than one argument passed
   if (arguments.length > 1) {
-    return console.log("Sorry, you may only pass one array of strings to mostFreqStr.");
+    return console.log('Sorry, you may only pass one array of strings to mostFreqStr.');
   }
   // the argument is not an array OR if it's empty
   if (!Array.isArray(arr) || arr.length < 1) {
-    return console.log("Sorry, you may only pass an array of strings to mostFreqStr.");
+    return console.log('Sorry, you may only pass an array of strings to mostFreqStr.');
   }
   // an element in arr is not a string
-  for (var i = 0; i < arr.length; i++) {
-    if (typeof arr[i] !== "string") {
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== 'string') {
       return console.log(`Sorry, element at index ${i} is not a string.`);
     }
   }
-  
-  var obj = {};
-  var mostFreq = 0;
-  var which = [];
 
+  const obj = {};
   // convert all strings to lower case
   const lcArr = [];
   arr.forEach(e => lcArr.push(e.toLowerCase()));
-
+  // count occurencies
   lcArr.forEach(ea => {
     if (!obj[ea]) {
       obj[ea] = 1;
     } else {
-      obj[ea]++;
-    }
-
-    if (obj[ea] > mostFreq) {
-      mostFreq = obj[ea];
-      which = [ea];
-    } else if (obj[ea] === mostFreq) {
-      which.push(ea);
+      obj[ea] += 1;
     }
   });
-  return which;
+  // array descending
+  return Object.keys(obj).sort((a, b) => obj[b] - obj[a]);
 }
 
 export const frequentPatterns = (transactions, query) => {
@@ -51,10 +44,12 @@ export const frequentPatterns = (transactions, query) => {
         // saves the plural versions rather than singular ones, covering a few irregular
         return (
           !(entry === query
-          || entry === query.slice(0, -1)
-          || entry === `${query}s`
-          || entry === `${query}ing`
-          || arr.includes(`${entry}s`)
+          || query.split(/\W/).includes(entry) // any terms in the query
+          || query.split(/\W/).map(e => e.slice(0, -2)).includes(`${entry}`)
+          || query.split(/\W/).map(e => `${e}ing`).includes(`${entry}`)
+          || query.split(/\W/).map(e => `${e}s`).includes(`${entry}`)
+          || query.split(/\W/).map(e => `${e}ed`).includes(`${entry}`)
+          || arr.includes(`${entry}s`) // variants of exisiting base terms
           || arr.includes(`${entry}ing`)
           || arr.includes(`${entry}oes`)
           || arr.includes(`${entry}en`)
@@ -67,8 +62,7 @@ export const frequentPatterns = (transactions, query) => {
       if (result.length) {
         resolve(result.slice(0, 5));
       } else resolve([]);
-    }
-    catch (e) {
+    } catch (e) {
       console.log(e);
       reject([]);
     }

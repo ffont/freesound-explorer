bf1d656d731db97ae0624ce83d3b90ec99c71efd
Eric Lehmann <coding@kernschall.de>
2021-05-05 21:48:50 +0200

    replaces node-sass with dart-sass and updates many dev-dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
 const precss = require('precss');
 const jsonImporter = require('node-sass-json-importer');
+// const sassVars = require('@epegzz/sass-vars-loader')
 
 function getEntrySources(sources) {
   if (process.env.NODE_ENV === 'flask') {
@@ -15,51 +16,73 @@ function getEntrySources(sources) {
   return sources;
 }
 
-function getPlugins(plugins) {
-  if (process.env.NODE_ENV !== 'production') {
-    plugins.push(new webpack.HotModuleReplacementPlugin());
-  }
-  if (process.env.NODE_ENV === 'production') {
-    plugins.push(new webpack.optimize.UglifyJsPlugin({
-      sourceMap: false,
-      mangle: false,
-    }));
-  }
-  return plugins;
-}
+// function getPlugins(plugins) {
+//   if (process.env.NODE_ENV !== 'production') {
+//     plugins.push(new webpack.HotModuleReplacementPlugin());
+//   }
+//   if (process.env.NODE_ENV === 'production') {
+//     plugins.push(new webpack.optimize.UglifyJsPlugin({
+//       sourceMap: false,
+//       mangle: false,
+//     }));
+//   }
+//   return plugins;
+// }
 
 module.exports = {
   devtool: (process.env.NODE_ENV !== 'production') ? 'eval' : '',
   entry: getEntrySources([
-    './src/index',
+    './src/index.jsx',
   ]),
   output: {
     path: path.join(__dirname, 'static/js/'),
     filename: 'bundle.js',
     publicPath: '/static/js/',
   },
-  plugins: getPlugins([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      },
-    }),
-    new webpack.optimize.OccurenceOrderPlugin(),
-  ]),
+  optimization: {
+    minimize: true,
+  },
+  // plugins: getPlugins([
+  //   new webpack.DefinePlugin({
+  //     'process.env': {
+  //       NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+  //     },
+  //   }),
+  // ]),
   module: {
-    loaders: [
+    rules: [
+      // {
+      //   test: /\.js?$/,
+      //   use: ['babel-loader'],
+      //   exclude: /node_modules/,
+      //   include: path.resolve(__dirname, 'src'),
+      // },
       {
         test: /\.jsx?$/,
-        loaders: ['babel'],
-        include: path.join(__dirname, 'src'),
+        use: ['babel-loader'],
+        include: path.resolve(__dirname, 'src'),
       },
       {
         test: /\.scss$/,
-        loaders: [
-          'style',
-          'css',
-          'postcss',
-          'sass',
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            }
+          },
+          'postcss-loader',
+          {
+            loader: "sass-loader",
+            options: {
+              // Prefer `dart-sass`
+              implementation: require("sass"),
+              sassOptions: {
+                importer: jsonImporter,
+              }
+            },
+          },
         ],
         include: [
           path.resolve(__dirname, 'src/components'),
@@ -68,9 +91,10 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loaders: [
-          'style',
-          'css',
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
         ],
       },
       {
@@ -81,20 +105,14 @@ module.exports = {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: 'url-loader?limit=10000&mimetype=application/font-woff',
       },
-      {
-        test: /\.json$/,
-        loader: 'json',
-      },
     ],
   },
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: [ '.js', '.jsx', '.scss']
+  //   enforceExtension: true,
+  // },
+  // postcss() {
+  //   return [autoprefixer, precss];
   },
-  postcss() {
-    return [autoprefixer, precss];
-  },
-  sassLoader: {
-    // Apply the JSON importer via sass-loader's options.
-    importer: jsonImporter,
-  },
-};
+}
+
